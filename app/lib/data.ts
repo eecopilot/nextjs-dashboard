fa6f974335e8b78a715e4d65c0d6560a28d0995f
eep <coolicer@foxmail.com>
2023-12-14 15:12:06 +0800

    feat: data fetching

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -9,6 +9,7 @@ import {
   Revenue,
 } from './definitions';
 import { formatCurrency } from './utils';
+// import { unstable_noStore } from 'next/cache';
 
 export async function fetchRevenue() {
   // Add noStore() here prevent the response from being cached.
